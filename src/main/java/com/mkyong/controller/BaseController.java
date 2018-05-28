c9d884a27000114013dda8d2cd05201a4b576e0f
Martin <Martin@DESKTOP-EUDQ3JC>
2018-05-28 11:15:42 +0800

    bendicangku

--- src/main/java/com/mkyong/controller/BaseController.java
+++ src/main/java/com/mkyong/controller/BaseController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class BaseController {
-
+	
 	private static int counter = 0;
 	private static final String VIEW_INDEX = "index";
 	private final static org.slf4j.Logger logger = LoggerFactory.getLogger(BaseController.class);
